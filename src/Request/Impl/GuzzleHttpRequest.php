9ff0dec6e818bc56f96ff86eeb4f88b8780576c9
nyatori nyatori <stevennight@dingtalk.com>
2023-05-18 16:20:53 +0800

    feat(master): test

--- src/Request/Impl/GuzzleHttpRequest.php
+++ src/Request/Impl/GuzzleHttpRequest.php
@@ -2,48 +2,136 @@
 
 namespace Stevennight\FeishuCorpSdk\Request\Impl;
 
+use Exception;
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+use Stevennight\FeishuCorpSdk\Exception\RequestException;
 use Stevennight\FeishuCorpSdk\Request\Request;
 
 class GuzzleHttpRequest extends Request
 {
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->client = new Client();
     }
 
     public function get()
     {
-        // TODO: Implement get() method.
+        try {
+            $res = $this->client->get($this->url, [
+                'query' => $this->query,
+            ]);
+        } catch (Exception $exception) {
+            // todo::这里没办法用原来的exception定位文件、line、trace等。
+            throw new RequestException($exception->getMessage());
+        }
+
+        return $this->handleResponse($res);
     }
 
     public function post()
     {
-        // TODO: Implement post() method.
+        try {
+            $res = $this->client->post($this->url, [
+                'query' => $this->query,
+                'json' => $this->json,
+                'form_params' => $this->form,
+            ]);
+        } catch (Exception $exception) {
+            throw new RequestException($exception->getMessage());
+        }
+
+        return $this->handleResponse($res);
     }
 
     public function delete()
     {
-        // TODO: Implement delete() method.
+        try {
+            $res = $this->client->delete($this->url, [
+                'query' => $this->query,
+                'json' => $this->json,
+                'form_params' => $this->form,
+            ]);
+        } catch (Exception $exception) {
+            throw new RequestException($exception->getMessage());
+        }
+
+        return $this->handleResponse($res);
     }
 
     public function put()
     {
-        // TODO: Implement put() method.
+        try {
+            $res = $this->client->put($this->url, [
+                'query' => $this->query,
+                'json' => $this->json,
+                'form_params' => $this->form,
+            ]);
+        } catch (Exception $exception) {
+            throw new RequestException($exception->getMessage());
+        }
+
+        return $this->handleResponse($res);
     }
 
     public function patch()
     {
-        // TODO: Implement patch() method.
+        try {
+            $res = $this->client->patch($this->url, [
+                'query' => $this->query,
+                'json' => $this->json,
+                'form_params' => $this->form,
+            ]);
+        } catch (Exception $exception) {
+            throw new RequestException($exception->getMessage());
+        }
+
+        return $this->handleResponse($res);
     }
 
     public function head()
     {
-        // TODO: Implement head() method.
+        try {
+            $res = $this->client->head($this->url, [
+                'query' => $this->query,
+                'json' => $this->json,
+                'form_params' => $this->form,
+            ]);
+        } catch (Exception $exception) {
+            throw new RequestException($exception->getMessage());
+        }
+
+        return $this->handleResponse($res);
     }
 
     public function options()
     {
-        // TODO: Implement options() method.
+        // todo::不知道是否可用
+        try {
+            $res = $this->client->options($this->url, [
+                'query' => $this->query,
+                'json' => $this->json,
+                'form_params' => $this->form,
+            ]);
+        } catch (Exception $exception) {
+            throw new RequestException($exception->getMessage());
+        }
+
+        return $this->handleResponse($res);
+    }
+
+    /**
+     * 处理响应
+     * @throws RequestException
+     */
+    public function handleResponse(ResponseInterface $res)
+    {
+        $body        = $res->getBody()->getContents();
+        $contentType = $res->getHeader('Content-Type')[0] ?? '';
+        if (stripos($contentType, 'application/json') !== false) {
+            $body = json_decode($body, true);
+        }
+        return $body;
     }
 }
