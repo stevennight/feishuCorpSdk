41f1198c2bab586b6d20d13787ea9cc162758802
nyatori nyatori <stevennight@dingtalk.com>
2023-08-30 11:11:17 +0800

    feat(master): 网页鉴权code2Session接口

---
+++ src/Api/OpenApis/Authen/V1/AccessTokenApi.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Stevennight\FeishuCorpSdk\Api\OpenApis\Authen\V1;
+
+use Stevennight\FeishuCorpSdk\Api\Api;
+use Stevennight\FeishuCorpSdk\Request\RequestMethod;
+
+/**
+ * 获取 user_access_token（网页应用）
+ *
+ * https://open.feishu.cn/document/server-docs/authentication-management/access-token/create-2
+ *
+ * AccessTokenApi
+ *
+ * @author Guo Junxian
+ * @date 2023/8/30
+ */
+class AccessTokenApi extends Api
+{
+    public $path = '/open-apis/authen/v1/access_token';
+    public $method = RequestMethod::REQUEST_METHOD_POST;
+    public $requestAccessToken = true;
+    public $grantType = 'grant_type';
+    public $code = '';
+
+    public function getJson()
+    {
+        $json = [
+            'grant_type' => $this->grantType,
+            'code' => $this->code,
+        ];
+        return $json;
+    }
+}
